fb843cc38406507866d7019859a54ad8c27a291b
Justin Frazer <rippleclips@gmail.com>
2019-10-24 15:25:21 -0600

    Login Screen Designed

--- Ripple_SwiftUI/ContentView.swift
+++ Ripple_SwiftUI/ContentView.swift
@@ -10,10 +10,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Text("Hello World")
-            Text("Hello World")
+        VStack {
+            VStack {
+                Image("Ripple Icon").resizable()
+                .frame(width: 130.0, height: 130.0)
+            }
+            .frame(height: UIScreen.main.bounds.height/2)
+            VStack(spacing: 5) {
+
+                TextField("Email or Phone Number", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .foregroundColor(.black)
+                Button(action: {
+                    print("Login Tapped")
+                }) {
+                    Text("Login")
+                        .foregroundColor(.white)
+                }
+                    .padding(5)
+                    .cornerRadius(30)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.white, lineWidth: 2)
+                    )
+                Spacer()
+                Text("Create an Account")
+                    .bold()
+                    .foregroundColor(.white)
+                Text("Forgot Your Password?")
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+                }
+            .padding(.horizontal, 20)
         }
+        .background(RadialGradient(gradient: Gradient(colors: [Color("RippleColorDark"), Color.black]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/, endRadius: /*@START_MENU_TOKEN@*/500/*@END_MENU_TOKEN@*/))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
