5c1653a1c166f74ab9bdf86347fc9e59b624c128
Justin Frazer <rippleclips@gmail.com>
2019-10-24 12:26:53 -0600

    Initial Commit

---
+++ Ripple_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ripple_SwiftUI
+//
+//  Created by justin frazer on 10/24/19.
+//  Copyright © 2019 justin frazer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
