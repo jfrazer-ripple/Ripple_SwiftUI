29d9c595cf95428e69d170fb84cfe57518cf9158
Justin Frazer <rippleclips@gmail.com>
2019-10-25 20:46:01 -0600

    Modified to display login status

--- Ripple_SwiftUI/LoginScreen.swift
+++ Ripple_SwiftUI/LoginScreen.swift
@@ -10,8 +10,10 @@ import SwiftUI
 
 struct LoginScreen: View {
     
-    @State var username: String = ""
-    @State var password: String = ""
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var passwordStatus = 0
+    @State private var usernameOrEmailStatus = 0
     
     @Binding var userID: Int
     
@@ -23,30 +25,82 @@ struct LoginScreen: View {
             }
             .frame(height: UIScreen.main.bounds.height/2)
             VStack(spacing: 5) {
-
-                TextField("Email or Username", text: $username)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(5)
-                .cornerRadius(30)
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                    .stroke(Color.white, lineWidth: 2)
-                )
-                SecureField("Password", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .foregroundColor(.black)
-                .padding(5)
-                .cornerRadius(30)
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                    .stroke(Color.white, lineWidth: 2)
-                )
+                if usernameOrEmailStatus == 0 { //Default value, user hasn't attempted to log in
+                    TextField("Email or Username", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(5)
+                        .cornerRadius(30)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.white, lineWidth: 2)
+                        )
+                } else if usernameOrEmailStatus == 1 { //User attempted to login, but the email/username was not found
+                    TextField("Email or Username", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(5)
+                        .cornerRadius(30)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.red, lineWidth: 2)
+                        )
+                }  else if usernameOrEmailStatus == 2 { //User has a valid email/username
+                    TextField("Email or Username", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(5)
+                        .cornerRadius(30)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.green, lineWidth: 2)
+                        )
+                }
+                if passwordStatus == 0 { //Default value, user hasn't attempted to log in
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(5)
+                        .cornerRadius(30)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.white, lineWidth: 2)
+                        )
+                } else if passwordStatus == 1 { //User attempted to login, but the password was not correct
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(5)
+                        .cornerRadius(30)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.red, lineWidth: 2)
+                        )
+                }  else if passwordStatus == 2 { //User has a valid email/username with the corresponding password
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(5)
+                        .cornerRadius(30)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.green, lineWidth: 2)
+                        )
+                }
                 Button(action: {
                     let loginAttempt = self.attemptLogin(str: self.username, pass: self.password)
-                    if  loginAttempt != 0 { //Successful Login
-                        self.userID = loginAttempt
+                    if loginAttempt == 0 { // username/email exists, but the password was incorrect
+                        print("Invalid password")
+                        self.usernameOrEmailStatus = 2
+                        self.passwordStatus = 1
+                    } else if loginAttempt == -1 { // unable to find the user given the email/username
+                        print("unkown username/email")
+                        self.usernameOrEmailStatus = 1
+                        self.passwordStatus = 0
+                    } else {
+                        //self.userID = loginAttempt
+                        self.usernameOrEmailStatus = 2
+                        self.passwordStatus = 2
                     }
                     
                     
@@ -83,7 +137,6 @@ struct LoginScreen: View {
         - Returning -1 means the username/email does not exist
     */
     func attemptLogin(str: String, pass: String) -> Int {
-        
         for user in myDataBase.allUsers {
             if user.email == str || user.username == str { //we found an entry for given email/username
                 if user.password == pass { //Passwords match, allow user to login
@@ -96,6 +149,7 @@ struct LoginScreen: View {
         return -1 //Unable to find the user with the given username/email, return 0
     }
 }
+
 
 //struct LoginScreen_Previews: PreviewProvider {
 //    static var previews: some View {
